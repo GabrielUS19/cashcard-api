f7f34936e52d81178196132022904bab50c44fb9
GabrielUS19 <gabrieluchoa2607@gmail.com>
2026-06-11 15:00:38 -0300

    chore: initial spring boot setup

---
+++ src/test/java/com/gabriel/cashcard_api/CashcardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.cashcard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
