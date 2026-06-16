8b023aaa5fa664181efcde6ff4689c8b9d7b4d54
GabrielUS19 <gabrieluchoa2607@gmail.com>
2026-06-16 14:30:01 -0300

    feat: Implements the Find By ID route

--- src/main/java/com/gabriel/cashcard_api/services/CashcardService.java
+++ src/main/java/com/gabriel/cashcard_api/services/CashcardService.java
@@ -6,6 +6,8 @@ import com.gabriel.cashcard_api.models.CashcardModel;
 import com.gabriel.cashcard_api.repositories.CashcardRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CashcardService {
     private final CashcardRepository cashcardRepository;
@@ -21,5 +23,12 @@ public class CashcardService {
         var savedCashcard = cashcardRepository.save(cashcardModel);
 
         return new CashcardResponse(savedCashcard.getId(), savedCashcard.getAmount());
+    }
+
+    public CashcardResponse findById(UUID id) {
+        return cashcardRepository
+                .findById(id)
+                .map(model -> new CashcardResponse(model.getId(), model.getAmount()))
+                .orElseThrow(() -> new IllegalArgumentException("Cashcard não encontrado com o ID: " + id));
     }
 }
